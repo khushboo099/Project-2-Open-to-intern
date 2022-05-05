34bde82566c0701021453621d0a1890d1b713499
Pritam0044 <you@example.com>
2022-05-05 11:23:39 +0530

    By Pritam

--- src/Controllers/CollegeController.js
+++ src/Controllers/CollegeController.js
@@ -33,7 +33,7 @@ const college = async function (req, res) {
     }
 
     if (
-      !/[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/.test(
+      !/[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)/.test(
         body.logoLink
       )
     ) {
